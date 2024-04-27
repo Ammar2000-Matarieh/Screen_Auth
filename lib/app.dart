3aca9fdf4bdc8efe580d6fd8ffddff31e866f75d
Ammar2000-Matarieh <ammarmataerieh2@gmail.com>
2024-04-27 19:41:57 +0300

    Update Project 2024

--- lib/app.dart
+++ lib/app.dart
@@ -5,10 +5,8 @@ class AppRun extends StatelessWidget {
   const AppRun({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Screen Auth App',
-        home: LoginScreenMain());
-  }
+  Widget build(BuildContext context) => const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Screen Auth App',
+      home: LoginScreenMain());
 }
